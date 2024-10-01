baca247c24c125c958c929ba1dc8b7ea744ee1b6
Yarla Aguiar <119678710+yarlaaguiar@users.noreply.github.com>
2024-10-01 11:56:43 -0300

    Projeto número da sorte adicionado

---
+++ logica-js-projeto_inicial/exercicios-treinamento/treinamento.js
@@ -0,0 +1,13 @@
+//*alert("Bem-vindo ao meu site!");
+//var nome = "Lua";
+//let idade = 25;
+/*let numeroDeVendas = 50;
+console.log(numeroDeVendas);*/
+//let saldoDisponivel = 1000;
+//console.log(saldoDisponivel);
+
+/*let mensagemDeErro = 'Erro! Preencha todos os campos.';
+alert(mensagemDeErro);*/
+
+prompt('Olá, digite seu nome:');
+let nome = "Yarla";
